3a505e4a7d1ff948768536df858005378467cacf
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-08-28 18:10:05 +1000

    Professor Login UI complete - needs functionality

--- student-side-web/src/components/ProfessorSignupForm.js
+++ student-side-web/src/components/ProfessorSignupForm.js
@@ -1,6 +1,93 @@
+import MSLLogo from '../assets/Logo.png';
+import '../index.css';
+
 const ProfessorSignupForm = () => {
     return (
-        <div>PROF SIGN UP</div>
+        <div className="signup-form">
+            <div className="form-box">
+              <img /* Image */
+                className="form-logo"
+                src={MSLLogo} alt="Logo"
+              />
+
+              {/* Form Container */}
+              <section className="form-container">
+                <form action="#" className="form">
+                {/* First/surname input boxes wrapped in column container for 1x2 display */}
+                <div className="column">
+                  <div className="input-box">
+                      <label htmlFor="firstName">First Name</label>
+                      <input type="text" id="firstName" placeholder="Dayana" />
+                  </div>
+
+                    <div className="input-box">
+                      <label htmlFor="lastName">Last Name</label>
+                      <input type="text" id="lastName" placeholder="Torres" />
+                    </div>
+                </div>
+
+                {/* Email input box*/}
+                    <div className="input-box">
+                      <label htmlFor="email">Email</label>
+                      <input type="text" id="email" placeholder="dayana@example.com" />
+                    </div>
+
+                  {/* Select boxes wrapped in column container for 1x2 display */}
+                  <div className="column">
+                    <div className="input-box">
+                      <label htmlFor="school">School</label>
+                          <div className="form-select-box">
+                            <select>
+                              <option>Berklee College of Music</option>
+                              <option>The Royal Academy of Music</option>
+                              <option>The Julliard School</option>
+                              <option>Yale School of Music</option>
+                            </select>
+                        </div>
+                    </div>
+
+                        <div className="input-box">
+                          <label htmlFor="instrument">Instrument</label>
+                          <div className="form-select-box">
+                            <select>
+                              <option>Piano</option>
+                              <option>Trombone</option>
+                              <option>Cello</option>
+                              <option>Violin</option>
+                              <option>Tuba</option>
+                              <option>Brass</option>
+                            </select>
+                          </div>
+                        </div>
+                    </div>
+
+                  {/* Course input box*/}
+                  <div className="input-box">
+                      <label htmlFor="Course">Course</label>
+                      <input type="text" id="Course" placeholder="Piano Techniques" />
+                    </div>
+
+                  {/* Password input box*/}
+                    <div className="input-box">
+                      <label htmlFor="Password">Password</label>
+                      <input type="text" id="Password" placeholder="Music123" />
+                    </div>
+
+                  {/* Logout button */}
+                    <div className="form-login">
+                      <p>Already have an account? </p> <a href="/login" className="login-link">Log in</a>
+                    </div>
+
+                    {/* Signup button */}
+                    <button className="signup-button">SIGN UP</button>
+
+                    {/* For demo only */}
+                    <a className="signup-button" href='/home'>Demo signup</a>
+                </form>
+
+              </section>
+            </div>
+          </div>
     )
 }
 
