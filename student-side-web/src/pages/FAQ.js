ea000232bcc417450f3b8be9dd4352e09d4a7017
JackyLai327 <jackylai327@gamil.com>
2023-09-03 21:28:37 +1000

    hold on

--- student-side-web/src/pages/FAQ.js
+++ student-side-web/src/pages/FAQ.js
@@ -22,36 +22,69 @@ const FAQ = () => {
                 <div className="faq-category"><img src={categoryTitle} alt="placeholder" /></div>
         </div>
 
-        <div className="greeting-heading mt-5">Category/Topic</div>
-        <hr className="thick-hr" />
+        <div className="faq-container">
+            <div className="greeting-heading mt-5">Category/Topic</div>
+            <hr className="thick-hr" />
 
-        
-        <details className="faq-topic-details">
-            <summary className="faq-topic-summary">Common Question</summary>
-            details will be displayed here
-        </details>
+            
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">General</summary>
+                <ul className="question"> What is MuseSceneLab?
+                    <li className="answer">MuseSceneLab is an educational platform focused on music that offers a unique learning experience through Virtual Reality (VR), Augmented Reality (AR), and Artificial Intelligence. Our flagship product, MuseLab_Conductor, allows students to interact in a VR environment with an orchestra of avatars and then analyze their performance through educational and usage metrics on our web platform.</li>
+                </ul>
+            </details>
 
-        <hr />
+            <hr />
 
-        <details className="faq-topic-details">
-            <summary className="faq-topic-summary">Common Question</summary>
-            details will be displayed here
-        </details>
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">Features</summary>
+                
+            </details>
 
-        <hr />
+            <hr />
 
-        <details className="faq-topic-details">
-            <summary className="faq-topic-summary">Common Question</summary>
-            details will be displayed here
-        </details>
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">Technical Requirements</summary>
+                details will be displayed here
+            </details>
 
-        <hr />
+            <hr />
 
-        <details className="faq-topic-details">
-            <summary className="faq-topic-summary">Common Question</summary>
-            details will be displayed here
-        </details>
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">Platform Usage</summary>
+                details will be displayed here
+            </details>
 
+            <hr />
+
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">Security and Privacy</summary>
+                details will be displayed here
+            </details>
+
+            <hr />
+
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">Subscription and Pricing</summary>
+                details will be displayed here
+            </details>
+
+            <hr />
+
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">Support</summary>
+                
+            </details>
+
+            <hr />
+
+            <details className="faq-topic-details">
+                <summary className="faq-topic-summary">Updates and Improvements</summary>
+                details will be displayed here
+            </details>
+
+            <hr />
+        </div>
     </div>
     );
 }
